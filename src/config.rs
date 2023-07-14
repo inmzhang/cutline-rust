c312b5ae0618f18867152ed0fd4359ee76164313
inmzhang <zhangyiming21@mail.ustc.edu.cn>
2023-07-14 10:43:16 +0800

    add config read write test

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
+use std::path::Path;
+use std::fs::File;
+use anyhow::{Result, Ok};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct TopologyConfig {
     pub grid_width: u32,
     pub grid_height: u32,
@@ -21,7 +24,7 @@ impl Default for TopologyConfig {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct AlgorithmConfig {
     pub circuit_depth: u32,
     pub max_search_depth: u32,
@@ -38,8 +41,36 @@ impl Default for AlgorithmConfig {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Debug, Serialize, Deserialize, Default, PartialEq, Eq)]
 pub struct Config {
     pub topology: TopologyConfig,
     pub algorithm: AlgorithmConfig,
+}
+
+impl Config {
+    pub fn save_to_json(&self, path: &Path) -> Result<()> {
+        // create file if not exist
+        let file = File::create(path)?;
+        serde_json::to_writer_pretty(file, self)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_config_read_write() -> Result<()> {
+        let config = Config::default();
+
+        let dir = tempdir()?;
+        let path = dir.path().join("config.json");
+        config.save_to_json(path.as_path())?;
+
+        let config2: Config = serde_json::from_reader(File::open(path.as_path())?)?;
+        assert_eq!(config, config2);
+        Ok(())
+    }
 }
